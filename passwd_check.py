5a5e838ade365b9e1a074f558fbbf9bd64c4c21d
Azamat Abdbekov <azamat_abdybekov@epam.com>
2026-03-07 06:56:15 +0600

    formated using flake8

--- passwd_check.py
+++ passwd_check.py
@@ -1,20 +1,25 @@
 def has_digit(password):
-	return any(each_symbol.isdigit() for  each_symbol in password)
+    return any(each_symbol.isdigit() for each_symbol in password)
+
 
 def has_upper_letters(password):
-	return any(each_symbol.isupper() for each_symbol in password)
+    return any(each_symbol.isupper() for each_symbol in password)
+
 
 def has_lower_letters(password):
-	return any(each_symbol.islower() for each_symbol in password)
+    return any(each_symbol.islower() for each_symbol in password)
+
 
 def is_very_long(password):
-	return len(password) > 12
+    return len(password) > 12
+
 
 def has_symbols(password):
-	return any(
-		not each_symbol.isdigit() and not each_symbol.isalpha()
-		for each_symbol in password
-	)
+    return any(
+        not each_symbol.isdigit() and not each_symbol.isalpha()
+        for each_symbol in password
+    )
+
 
 def main():
     password = input('Введите пароль: ')
@@ -33,6 +38,7 @@ def main():
             score += 2
 
     print(f'Рейтинг пароля: {score}')
+
 
 if __name__ == "__main__":
     main()
